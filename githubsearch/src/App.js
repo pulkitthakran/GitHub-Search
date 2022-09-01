531771d8b3c495c609daa1021c632c3cfcdb6287
PULKIT <mukeshpulkit76@gmail.com>
2022-09-01 18:35:56 +0530

    search bar

--- githubsearch/src/App.js
+++ githubsearch/src/App.js
@@ -1,11 +1,44 @@
 import './App.css';
-import Header from './component/header';
+import Header from "./component/header";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Search from './component/seachBar';
+
 function App() {
-  return (
-    <>
-      <Header/>
-    </>
-  );
+    const [user, setUser] = useState('');
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false)
+
+    useEffect(()=>{
+        fetchUserData('pulkitthakran')
+    },[])
+    function handleSearch(username) {
+        setLoading(true)
+        setUser('')
+        fetchUserData(username)
+        console.log(username);
+    }
+
+    function fetchUserData(username) {
+        axios.get(  `https://api.github.com/users/${username}`)
+            .then((res) => {
+                setLoading(false)
+                setError(false)
+                setUser(res.data)
+            })
+            .catch(() => {
+                setLoading(false)
+                setError(true)
+            })
+    }
+    
+  
+    return (
+        <>
+            <Header/>         
+            <Search handleSearch={handleSearch} hasError={error} hasLoading= {loading}/>
+        </>
+    );
 }
 
 export default App;
